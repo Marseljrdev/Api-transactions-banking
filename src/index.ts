caf6925be8839d1c36d33c5f2fa5fa981464c58e
Marsel Brandao Junior <marseljrdev@gmail.com>
2023-06-25 21:31:50 -0300

    Ajustes para o deploy

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { UsersRoutes } from "./routes/users.routes";
 import { TransactionsRoutes } from "./routes/transactions.routes";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //USERS
 
@@ -13,6 +17,9 @@ app.use("/", UsersRoutes());
 
 app.use("/", TransactionsRoutes());
 
-app.listen(3333, () => {
-  console.log("Api is running port 3333...");
+app.listen(process.env.PORT, () => {
+  console.log("Api is running port " + process.env.PORT);
 });
+function cors(): any {
+  throw new Error("Function not implemented.");
+}
